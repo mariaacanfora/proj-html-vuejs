3cd9ddaef0f3459783783bc12ff26d7ca8607de7
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-11-22 17:42:56 +0100

    Finished layout structure

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,209 @@
         </div>
 
         <div class="container logo-container py-3 d-flex justify-content-between align-items-center">
-          <img src="@/assets/img/avada-movers-logo.png" alt="">
+          <img src="@/assets/img/avada-movers-logo.png" alt="movers logo">
           <div>
             <ul class="list-unstyled m-0 d-inline-block">
-              <li v-for="(item, i) in linkHeaderList" :key="i" class="d-inline-block px-4">
+              <li v-for="(item, i) in linkHeaderList" :key="'A' + i" class="d-inline-block px-4">
                 <a :href="item.href" class="text-decoration-none text-dark">{{item.link}}</a>
               </li>
             </ul>
-            <button class="btn rounded-pill btn-color">FREE QUOTE</button>
+            <button class="btn rounded-pill personal-btn">FREE QUOTE</button>
           </div>
         </div>
       </div>
     </header>
+
+    <main>
+      <div class="hero-section ">
+        <div class="container text-center">
+          <h4>MOVING WITHOUT THE HASSLE</h4>
+          <h1>WE MAKE IT SIMPLE</h1>
+          <h4>PROFESSIONAL SERVICE WITH QUALITY AND CUSTOMER SATISFACTION</h4>
+        </div>
+      </div>
+
+      <div class="free-quote-section">
+        <div class="container text-center text-light">
+          <h4><i class="fa fa-paper-plane-o" aria-hidden="true"></i> GET A FREE QUOTE ONLINE NOW!</h4>
+        </div>
+      </div>
+
+      <div class="winning-section general-section">
+        <div class="container text-center">
+          <h4>HEADACHE FREE</h4>
+          <div class="row justify-content-center">
+            <div class="col-4"><h2>Award Winning Moving Company</h2></div>
+            <div class="col-9"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos unde officiis dolorum deserunt sunt explicabo quidem aspernatur odit tempora vero molestiae quae voluptate tenetur iure repellat amet, labore rem! Nostrum.</p></div>
+          </div>
+          <button class="btn rounded-pill personal-btn">READ OUR TESTIMONIALS</button>
+          
+        </div>
+      </div>
+
+      <div class="full-service-section general-section">
+        <div class="container text-center">
+          <h4>WE ARE PROFESSIONAL</h4>
+          <h2>Full-Service Solution</h2>
+          <div class="row gx-5">
+            <div class="col">
+              <div class="p-3 bg-card">
+                <img src="@/assets/img/avada-movers-serviceonephoto-final-400x255.jpg" alt="two man teams">
+                <div class="card-txt">
+                  <h3>Two Man Teams</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptatibus corporis, quisquam necessitatibus minus ullam earum quis quo recusandae. Nisi qui consequuntur aliquam ducimus praesentium natus? Alias voluptatum accusantium numquam.</p>
+                  <button class="btn rounded-pill personal-btn">READ MORE</button>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="p-3 bg-card">
+                <img src="@/assets/img/avada-movers-serviceonephoto-final-400x255.jpg" alt="two man teams">
+                <div class="card-txt">
+                  <h3>Two Man Teams</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptatibus corporis, quisquam necessitatibus minus ullam earum quis quo recusandae. Nisi qui consequuntur aliquam ducimus praesentium natus? Alias voluptatum accusantium numquam.</p>
+                  <button class="btn rounded-pill personal-btn">READ MORE</button>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="p-3 bg-card">
+                <img src="@/assets/img/avada-movers-serviceonephoto-final-400x255.jpg" alt="two man teams">
+                <div class="card-txt">
+                  <h3>Two Man Teams</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptatibus corporis, quisquam necessitatibus minus ullam earum quis quo recusandae. Nisi qui consequuntur aliquam ducimus praesentium natus? Alias voluptatum accusantium numquam.</p>
+                  <button class="btn rounded-pill personal-btn">READ MORE</button>
+                </div>
+              </div>
+            </div>
+
+            
+
+            
+          </div>
+          
+        </div>
+      </div>
+
+      <div class="ease-section general-section">
+        <div class="container">
+          <div class="row justify-content-end">
+              <div class="col-6 text-center">
+                <div class="p-3 bg-card">
+                  <div class="card-txt">
+                    <h4>WE REMOVE THE HEADACHE FROM MOVING</h4>
+                    <h2>Move With Ease</h2>
+                    <p>With our super simple service we can take you from getting a quote, right through to being in your new home with all of your belongings securely packed and safely delivered, even in the rooms of your choice. </p>
+                    <button class="btn rounded-pill personal-btn">GET A FREE QUOTE NOW</button>
+                  </div>
+                </div>
+              </div>
+        </div>
+        </div>
+      </div>
+
+      <div class="testimonial-section general-section">
+        <div class="container text-center">
+          <h4>HAPPY CUSTOMERS</h4>
+          <h2>Testimonials</h2>
+          <div class="row">
+            <div class="col">
+              <div class="testimonial-container">
+                <img src="./assets/img/avada-movers-johndoe-final-200x200.jpg" alt="john doe" class="rounded-circle w-25">
+                <h3>John Doe</h3>
+                <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita accusamus mollitia aperiam nostrum laborum, beatae perferendis quae excepturi, consequuntur, debitis impedit adipisci delectus omnis magni minus. Quidem voluptatem mollitia molestiae!"</p>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="testimonial-container">
+                <img src="./assets/img/avada-movers-johndoe-final-200x200.jpg" alt="john doe" class="rounded-circle w-25">
+                <h3>John Doe</h3>
+                <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita accusamus mollitia aperiam nostrum laborum, beatae perferendis quae excepturi, consequuntur, debitis impedit adipisci delectus omnis magni minus. Quidem voluptatem mollitia molestiae!"</p>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="testimonial-container">
+                <img src="./assets/img/avada-movers-johndoe-final-200x200.jpg" alt="john doe" class="rounded-circle w-25">
+                <h3>John Doe</h3>
+                <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita accusamus mollitia aperiam nostrum laborum, beatae perferendis quae excepturi, consequuntur, debitis impedit adipisci delectus omnis magni minus. Quidem voluptatem mollitia molestiae!"</p>
+              </div>
+            </div>
+          </div>
+          <button class="btn rounded-pill personal-btn">READ MORE TESTIMONIALS</button>
+        </div>
+      </div>
+
+      <div class="request-section general-section">
+        <div class="container text-center">
+          <h2>
+            Request A Free Quote
+          </h2>
+          <div class="row justify-content-center">
+            <div class="col-7">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ut, praesentium omnis neque sint inventore pariatur possimus facilis nobis minima architecto labore ea, ipsa non magni itaque doloremque ad. Praesentium.</p>
+            </div>
+          </div>
+          <button class="btn rounded-pill personal-btn">CLICK HERE</button>
+        </div>
+      </div>
+    </main>
+
+    <footer>
+      <div class="general-section">
+        <div class="container">
+          <div class="row">
+            <div class="col-4">
+              <h4>
+                ABOUT
+              </h4>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, enim adipisci. Quasi incidunt similique dolor accusantium labore sequi exercitationem? Eaque voluptatibus quae obcaecati nisi? Aut laudantium earum quo numquam laborum?</p>
+              <img src="@/assets/img/avada-movers-logo.png" alt="movers logo">
+            </div>
+
+            <div class="col-4">
+              <h4>
+                AVADA MOVERS
+              </h4>
+              <ul class="list-unstyled">
+                <li v-for="(item, i) in linkHeaderList" :key="'B' + i" class="py-3">
+                  > <a :href="item.href" class="text-dark">{{item.link}}</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-4">
+              <h4>
+                RECENT POSTS
+              </h4>
+              <ul class="list-unstyled">
+                <li v-for="(item, i) in linkFooterList" :key="'C' + i" class="py-3">
+                  > <a :href="item.href" class="text-dark">{{item.link}}</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer-info-section">
+        <div class="container text-center">
+          <ul class="list-unstyled list-info">
+            <li v-for="(item, i) in infoFooterList" :key="'D'+ i" class="d-inline-block"> <i :class="item.iconClass" aria-hidden="true"></i> <a :href="item.href">{{item.link}}</a> <span v-if="item !== infoFooterList[infoFooterList.length - 1]"> |</span></li>
+          </ul>
+
+          <ul class="list-unstyled m-0">
+            <li v-for="(icon, i) in iconHeaderList" :key="'E' + i" class="d-inline-block px-2">
+              <a :href="icon.href"><i aria-hidden="true" :class="icon.classIcon" class="text-light"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      
+    </footer>
   </div>
 </template>
 
@@ -90,6 +281,60 @@ export default {
           link: "Contact",
           href: "/contact"
         },
+      ],
+
+      linkFooterList:[
+        {
+          link: "Heading Out To College?",
+          href:"/heading-out-to-college"
+        },
+
+        {
+          link: "Moving Your Business?",
+          href:"/moving-your-business"
+        },
+
+        {
+          link: "Outstanding Quality",
+          href:"/outstanding-quality"
+        },
+
+        {
+          link: "Cost of Moving",
+          href:"/cost-of-moving"
+        },
+        
+        {
+          link: "Best Moving Tips",
+          href:"/best-moving-tips"
+        },
+        
+      ],
+
+      infoFooterList: [
+        {
+          link: "Copyright 2012 - 2020",
+          href: "/copyright",
+          iconClass: "fa fa-copyright"
+        },
+
+        {
+          link: "Avada Theme by ThemeFusion",
+          href: "/avada",
+          iconClass: "d-none"
+        },
+
+        {
+          link: "All Rights Reserved",
+          href: "/rights-reserved",
+          iconClass: "d-none"
+        },
+
+        {
+          link: "Powered by WordPress",
+          href: "/wordpress",
+          iconClass: "d-none"
+        },
       ]
     }
   }
@@ -99,35 +344,184 @@ export default {
 <style lang="scss">
 @import '~bootstrap/scss/bootstrap';
 @import '@/styles/variables.scss';
+@import '@/styles/reset.scss';
 @import '~font-awesome/css/font-awesome.css';
 
+
+
+body{
+  
+  font-weight: bold;
+
+  h1{
+    font-size: 3rem;
+    font-weight: bold;
+  }
+
+  h2{
+    font-size: 2.5rem;
+    font-weight: bold;
+    padding-bottom: 2rem;
+    color: $lima;
+  }
+
+  h3{
+    font-size: 2rem;
+    font-weight: bold;
+    color: $denim;
+    padding: 1rem;
+  }
+
+  h4{
+    font-size: 0.85rem;
+    font-weight: bold;
+    padding-bottom: 0.75rem;
+    color: $silver-chalince;
+  }
+
+  p{
+    font-weight: normal;
+    padding-bottom: 2rem;
+  }
+
+  .personal-btn{
+    background-color: $denim;
+    color: white;
+    padding: 0.375rem 1.25rem;
+    font-weight: bold;
+  }
+}
+
+//HEADER
 header{
   position: relative;
+  
+  .header-info{
+    background-color: $lima;
+  }
+  
+  .logo-container{
+    position: absolute;
+    top: 40px;
+    left: 0;
+    right: 0;
+  }
 }
 
-.header-info{
-  background-color: $lima;
+//MAIN
+
+main{
+  .hero-section{
+  background-image: url('./assets/img/avada-movers-homeherobackground-final.jpg');
+  background-size: 110%;
+  background-position: inherit;
+  padding-top: $pt-hero;
+  padding-bottom: $pb-hero;
 }
 
-.logo-container{
-  position: absolute;
-  top: 40px;
-  left: 0;
-  right: 0;
-}
-
-.btn-color{
+.free-quote-section{
+  padding: $py-base 0;
   background-color: $denim;
-  color: white;
-  padding: 0.375rem 1.25rem;
 }
 
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+.general-section{
+  padding: $py-section 0;
+}
+
+.winning-section{
+  background-color: $mercury;
+}
+
+.full-service-section{
+  
+  img{
+    width: 100%;
+    margin-bottom: 1rem;
+  }
+
+
+  .bg-card{
+    background-color: #F2F5F8;
+  }
+  .card-txt{
+    background-color: white;
+
+    p{
+      padding: 0 0.5rem
+    }
+
+    .personal-btn{
+      margin: 2rem 0;
+    }
+    
+  }
+}
+
+.ease-section{
+  background-image: url('./assets/img/avada-movers-movewithease-background-new.jpg');
+  background-size: cover;
+  background-position: top;
+
+  .bg-card{
+    background-color: $mantis;
+    border: 15px solid #5AA511;
+  }
+
+  .card-txt{
+    h2, h4{
+    color: white;
+    }
+    color: white;
+    padding: 2rem;
+  }
+}
+
+.testimonial-section{
+  background-image: url('./assets/img/avada-movers-testimonial-background.jpg');
+  background-size: cover;
+  background-position: top;
+
+  .personal-btn{
+    margin-top: 3rem;
+  }
+}
+
+.request-section{
+  background-image: url('./assets/img/avada-movers-request-quote-background.jpg');
+  background-size: cover;
+  background-position: top;
+  border-bottom: 15px solid $lima;
+}
+}
+
+//FOOTER
+footer{
+  a{
+    text-decoration: none;    
+    font-weight: normal;
+    font-size: 1.25rem;
+  }
+
+  .general-section{
+    padding: $py-base 0;
+  }
+
+  .footer-info-section{
+    background-color: $lima;
+    padding: $py-base 0;
+    color: white;
+    font-weight: normal;
+
+    .list-info{
+      a{
+        color: white;
+        font-size: 0.85rem;
+        padding: 0 0.25rem;
+      }
+
+    }
+
+  }
+}
+
 </style>
